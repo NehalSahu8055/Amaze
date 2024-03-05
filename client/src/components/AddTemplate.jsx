ce06e30e97d1f9f46595ac5fb7314e31c223beb2
Nehal Sahu <nehal@amazesoftwaretech.com>
2024-03-05 11:20:32 +0530

    add: AddTemplate Form

--- client/src/components/AddTemplate.jsx
+++ client/src/components/AddTemplate.jsx
@@ -1,6 +1,47 @@
-import { Button } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import React, { useRef } from "react";
+
 function AddTemplate() {
+  const entityArray = ["1780s890384093", "e128937197491824n"];
+  const senderArray = ["1780s890384093", "e128937197491824n"];
+  const transactionArray = ["Transaction", "Service", "Promotion"];
+  const languageArray = [
+    "AMHARIC",
+    "ARABIC",
+    "BENGALI",
+    "CHINESE",
+    "GREEK",
+    "GUJARATI",
+    "HINDI",
+    "KANNADA",
+    "MALAYALAM",
+    "MARATHI",
+    "NEPALI",
+    "ORIYA",
+    "PERSIAN",
+    "PUNJABI",
+    "RUSSIAN",
+    "SANSKRIT",
+    "SERBIAN",
+    "SINHALESE",
+    "TAMIL",
+    "TELUGU",
+    "TIGRINYA",
+    "URDU",
+  ];
+
   const templateForm = useRef(null);
   const handletemplate = (e) => {
     e.preventDefault();
@@ -20,43 +61,89 @@ function AddTemplate() {
     >
       <h3 className="text-xl font-semibold">Add Template</h3>
 
-      <select
-        id="entity-template"
-        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
-      >
-        // TODO: populate col
-        <option selected>Entity</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-      <select
-        id="entity-template"
-        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
-      >
-        // TODO: populate sender col
-        <option selected>MTCOEM</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
-      <select
-        id="entity-template"
-        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
-      >
-        <option selected>Selected Template Category</option>
-        <option value="transaction">Transaction</option>
-        <option value="service">Service</option>
-        <option value="promotion">Promotion</option>
-      </select>
-      <Button
-        type="submit"
-        className="mt-2 w-fit  bg-slate-800 px-4 capitalize text-white hover:bg-slate-800/90"
-      >
-        Save Now
+      <Autocomplete
+        className="w-full"
+        size="small"
+        disablePortal
+        id="selectEntity"
+        options={entityArray}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Select Entity" />
+        )}
+      />
+      <Autocomplete
+        className="w-full"
+        size="small"
+        disablePortal
+        id="selectSender"
+        options={senderArray}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Select Sender" />
+        )}
+      />
+      <Autocomplete
+        className="w-full"
+        size="small"
+        disablePortal
+        id="selectTransaction"
+        options={transactionArray}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Select Template Category" />
+        )}
+      />
+
+      <TextField
+        id="dlt-template-ID"
+        name="dlt-template-ID"
+        label="DLT Template ID"
+        size="small"
+      />
+      <TextField
+        id="title"
+        name="title"
+        label="Title"
+        variant="outlined"
+        size="small"
+      />
+      <Button className="mt-2 w-fit  bg-sky-600 px-4 capitalize text-white hover:bg-sky-600/90">
+        Add Variable
       </Button>
+      <Box className="flex gap-6">
+        <Autocomplete
+          className="w-full"
+          size="small"
+          disablePortal
+          id="selectLanguage"
+          options={languageArray}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Select Language" />
+          )}
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="Enable"
+        />
+      </Box>
+
+      <Box className="space-x-3">
+        <Button
+          type="submit"
+          className="mt-2 w-fit  bg-slate-800 px-4 capitalize text-white hover:bg-slate-800/90"
+        >
+          Save Template
+        </Button>
+        <Button
+          type="reset"
+          variant="outlined"
+          className="mt-2 w-fit  border-slate-800  px-4 capitalize text-slate-800 hover:border-slate-800 hover:bg-slate-800 hover:text-white"
+        >
+          Reset
+        </Button>
+      </Box>
     </form>
   );
 }
--- client/src/components/Profile/Templates.jsx
+++ client/src/components/Profile/Templates.jsx
@@ -2,11 +2,53 @@ import React from "react";
 import AddTemplate from "../AddTemplate";
 
 function Templates() {
+  { field: "id", headerName: "Sr.", width: 50 },
+  { field: "sender", headerName: "Sender", width: 140 },
+  { field: "entity", headerName: "Entity", width: 140 },
+  { field: "approvedOn", headerName: "Approved On", width: 120 },
+  { field: "status", headerName: "Status", width: 100 },
+  { field: "action", headerName: "Action", width: 80 },
+];
+const rows = [
+  {
+    id: 1,
+    sender: "MTCOM",
+    entity: "170******714",
+    approvedOn: "28 Feb 2024",
+    status: "APPROVED",
+    action: "🗑️",
+  },
+  {
+    id: 2,
+    sender: "MTCOM",
+    entity: "170******714",
+    approvedOn: "28 Feb 2024",
+    status: "APPROVED",
+    action: "🗑️",
+  },
+  {
+    id: 3,
+    sender: "MTCOM",
+    entity: "170******714",
+    approvedOn: "28 Feb 2024",
+    status: "APPROVED",
+    action: "🗑️",
+  },
+  {
+    id: 4,
+    sender: "MTCOM",
+    entity: "170******714",
+    approvedOn: "28 Feb 2024",
+    status: "APPROVED",
+    action: "🗑️",
+  },
+];
   return (
     <div>
       <h2 className="pb-8 text-3xl font-semibold tracking-tight  text-slate-950">
         Manage Template
       </h2>
+      
       <AddTemplate />
     </div>
   );
